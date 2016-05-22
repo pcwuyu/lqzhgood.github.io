7d4fde97820bcbd28ba7730eafa306c2b69845da
lqzhgood <liuqinzhi@live.cn>
2016-05-22 22:54:58 +0800

    Site updated: 2016-05-22 22:54:56

---
+++ about/js/main.js
@@ -0,0 +1,40 @@
+window.onload = function () {
+    
+    $("#wrap").appendTo("body");
+
+    var lmsArray = [];
+    var lmsArrayTure = "73&83&71&78&69&77&85&73&76";
+    var timer;
+
+    $("body").keyup(function (e) {
+        clearTimeout(timer);
+
+        var key = e.which;
+        if (key == 27 && $("#heart").css("opacity") == 1) {
+            $(".colorEgg").animate({
+                opacity: 0
+            }, 1000);
+        }
+        lmsArray.unshift(key);
+
+        var lmsStr = lmsArray.join("&");
+        if (lmsStr.indexOf(lmsArrayTure) != -1) {
+            lmsArray = [];
+            $("#wrap").animate({
+                opacity: 0.7
+            }, 1000);
+            $("#heart").animate({
+                opacity: 1
+            }, 1000);
+        }
+
+        timer = setTimeout(function () {
+            lmsArray = [];
+        }, 2000);
+
+        console.log(lmsArray);
+
+    });
+
+
+}
